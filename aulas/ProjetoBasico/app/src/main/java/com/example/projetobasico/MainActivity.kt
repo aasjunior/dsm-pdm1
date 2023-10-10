717d71b357e099914ddb14d4ce15c170e09f92fb
aasjunior <adeldivo@hotmail.com>
2023-10-10 19:16:50 -0300

    aula-09_10_2023/Criação do projeto básico

--- aulas/ProjetoBasico/app/src/main/java/com/example/projetobasico/MainActivity.kt
+++ aulas/ProjetoBasico/app/src/main/java/com/example/projetobasico/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.projetobasico
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+         * A variável `savedInstanceState` guarda informações do
+         * estado da Activity, guarda quais	 componentes estão na tela e
+         * informações que o usuário tenha preenchido.
+        */
+
         super.onCreate(savedInstanceState)
+        val tex = findViewById<TextView>("textView")
+
+        // os recursos contidos na pasta `res` são tornados em atributos em tempo de compilação (metaprogramação)
         setContentView(R.layout.activity_main)
     }
 }
